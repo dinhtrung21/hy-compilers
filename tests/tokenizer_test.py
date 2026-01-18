adffff13906c773859dc38e5ae80571766905a63
Trung Nguyen <115093361+dinhtrung21@users.noreply.github.com>
2026-01-18 12:30:06 +0000

    update tests

--- tests/tokenizer_test.py
+++ tests/tokenizer_test.py
@@ -2,15 +2,15 @@ from compiler.tokenizer import tokenize, Token, Location, L
 
 def test_tokenizer_basics() -> None:
     assert tokenize("abc123_") == [
-        Token(loc=L, type="identifier", text="abc123_")
+        Token(location=L, type="identifier", text="abc123_")
     ]
     
 def test_tokenizer_full() -> None:
     assert tokenize("(if 3 # this is a comment\nwhile*)") == [
-        Token(loc=Location(row=0, column=0), type="punctuation", text="("),
-        Token(loc=Location(row=0, column=1), type="identifier", text="if"),
-        Token(loc=Location(row=0, column=4), type="int_literal", text="3"),
-        Token(loc=Location(row=1, column=0), type="identifier", text="while"),
-        Token(loc=Location(row=1, column=5), type="operator", text="*"),
-        Token(loc=Location(row=1, column=6), type="punctuation", text=")")
+        Token(location=Location(row=0, column=0), type="punctuation", text="("),
+        Token(location=Location(row=0, column=1), type="identifier", text="if"),
+        Token(location=Location(row=0, column=4), type="int_literal", text="3"),
+        Token(location=Location(row=1, column=0), type="identifier", text="while"),
+        Token(location=Location(row=1, column=5), type="operator", text="*"),
+        Token(location=Location(row=1, column=6), type="punctuation", text=")")
     ]
