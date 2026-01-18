f4faef850008c56675f1c410e122c0b84ed91d52
Trung Nguyen <115093361+dinhtrung21@users.noreply.github.com>
2026-01-18 15:44:46 +0000

    multi-case test for task 1

--- tests/parser_test.py
+++ tests/parser_test.py
@@ -2,10 +2,27 @@ import compiler.ast as ast
 from compiler.parser import parse
 from compiler.tokenizer import tokenize, Token, Location, L
 
-def test_parser_basics() -> None:
-    tokens = tokenize("a + 3")
+
+def test_parser_task1() -> None:
+    tokens = tokenize('9 - (a + b - 3) * f / c')
     assert parse(tokens) == ast.BinaryOp(
-        left=ast.Identifier(name="a"),
-        op="+",
-        right=ast.Literal(value=3)
+        left=ast.Literal(value=9),
+        op="-",
+        right=ast.BinaryOp(
+            left=ast.BinaryOp(
+                left=ast.BinaryOp(
+                    left=ast.BinaryOp(
+                        left=ast.Identifier(name="a"),
+                        op="+",
+                        right=ast.Identifier(name="b")
+                    ),
+                    op="-",
+                    right=ast.Literal(value=3)
+                ),
+                op="*",
+                right=ast.Identifier(name="f")
+            ),
+            op="/",
+            right=ast.Identifier(name="c")
+        )
     )
