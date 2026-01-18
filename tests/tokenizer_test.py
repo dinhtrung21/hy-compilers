447e43f4798b0b25302cbaa490f7a08025d1245b
Trung Nguyen <115093361+dinhtrung21@users.noreply.github.com>
2026-01-18 07:53:24 +0000

    separate to different tests

--- tests/tokenizer_test.py
+++ tests/tokenizer_test.py
@@ -1,6 +1,16 @@
-from compiler.tokenizer import tokenize, Token, L
+from compiler.tokenizer import tokenize, Token, Location, L
 
 def test_tokenizer_basics() -> None:
-    assert tokenize("if  3\nwhile") == ['if', '3', 'while']
-    assert tokenize("123abc456def") == ['123', 'abc', '456', 'def']
-    assert tokenize("3-2 is ok") == ['3', '-', '2', 'is', 'ok']
+    assert tokenize("abc123_") == [
+        Token(loc=L, type="identifier", text="abc123_")
+    ]
+    
+def test_tokenizer_full() -> None:
+    assert tokenize("(if 3 # this is a comment\nwhile*)") == [
+        Token(loc=Location(row=0, column=0), type="punctuation", text="("),
+        Token(loc=Location(row=0, column=1), type="identifier", text="if"),
+        Token(loc=Location(row=0, column=4), type="int_literal", text="3"),
+        Token(loc=Location(row=1, column=0), type="identifier", text="while"),
+        Token(loc=Location(row=1, column=5), type="operator", text="*"),
+        Token(loc=Location(row=1, column=6), type="punctuation", text=")")
+    ]
