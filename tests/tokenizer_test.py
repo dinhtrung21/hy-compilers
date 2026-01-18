3e4f70847991d43c29236c76ad583f3ffb11f6ff
Trung Nguyen <115093361+dinhtrung21@users.noreply.github.com>
2026-01-18 15:44:21 +0000

    update test for tokenizer

--- tests/tokenizer_test.py
+++ tests/tokenizer_test.py
@@ -7,10 +7,10 @@ def test_tokenizer_basics() -> None:
     
 def test_tokenizer_full() -> None:
     assert tokenize("(if 3 # this is a comment\nwhile*)") == [
-        Token(location=Location(row=0, column=0), type="punctuation", text="("),
-        Token(location=Location(row=0, column=1), type="identifier", text="if"),
-        Token(location=Location(row=0, column=4), type="int_literal", text="3"),
-        Token(location=Location(row=1, column=0), type="identifier", text="while"),
-        Token(location=Location(row=1, column=5), type="operator", text="*"),
-        Token(location=Location(row=1, column=6), type="punctuation", text=")")
+        Token(location=Location(row=1, column=0), type="punctuation", text="("),
+        Token(location=Location(row=1, column=1), type="identifier", text="if"),
+        Token(location=Location(row=1, column=4), type="int_literal", text="3"),
+        Token(location=Location(row=2, column=0), type="identifier", text="while"),
+        Token(location=Location(row=2, column=5), type="operator", text="*"),
+        Token(location=Location(row=2, column=6), type="punctuation", text=")")
     ]
